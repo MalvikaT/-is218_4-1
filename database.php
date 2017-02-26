413a5421655d8e34af6f93bff4e9bf33538b04c1
malvika tripathi <mt394@afsconnect1.njit.edu>
2017-02-26 14:09:46 -0500

    some changes

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-$username = 'mgs_user';
-$password = 'pa55word';
+$dsn = 'mysql:host=sql2.njit.edu;dbname=mt394';
+$username = 'mt394';
+$password = '8CVY9DrV';
 
-try {
-	$db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('database_error.php');
-        exit();
-    }
+	try {
 
+		$db = new PDO($dsn, $username, $password);
+	} catch (PDOException $e) {
+		$error_message = $e->getMessage();
+		include('database_error.php');
+		exit();
+	}	
 ?>
